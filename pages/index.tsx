8bec2e35d3e0719572295af2af208f764c963b3e
alexjenkinsynwa@gmail.com <alexjenkins@Alexs-MacBook-Pro.local>
2022-11-28 15:42:07 -0500

    tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+//mx axis set to auto margin bottom is 8 see tailwind docs.
+export default function Home () {
+  return (
+    <div className="container mx-auto px-1- mb-8"> 
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      </div>
+
+  )
+  }
