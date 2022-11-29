408c586a8921a5cf68c4d867b3ca242cbeae106e
alexjenkinsynwa@gmail.com <alexjenkins@Alexs-MacBook-Pro.local>
2022-11-29 14:16:55 -0500

    added components folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,33 @@
 import Head from 'next/head'
 
+//
+const posts = [
+  { title: 'React Testing', excerpt: 'React Testing' },
+  { title: 'React', excerpt: 'React' }, 
+  
+]
+
 //mx axis set to auto margin bottom is 8 see tailwind docs.
 export default function Home () {
   return (
     <div className="container mx-auto px-1- mb-8"> 
       <Head>
-        <title>Create Next App</title>
+        <title>The Dev Spot Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        <div>
+        {posts.map((post, index) => (
+            <div>
+              {post.title}
+              {post.excerpt}
+              </div>
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+      </div>
+
+      </div>
 
       </div>
 
