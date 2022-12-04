ae37f905490f12e41e6591fc0e5732259532f7ed
alexjenkinsynwa@gmail.com <alexjenkins@Alexs-MacBook-Pro.local>
2022-12-03 22:25:45 -0500

    Header added

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import { Layout } from '../componets';
+import 'tailwindcss/tailwind.css';
 import '../styles/globals.scss';
-// import 'tailwindcss/tailwind.css';
 // import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return 
+  <Layout>
+<Component {...pageProps} />
+</Layout>
 }
 
 export default MyApp
